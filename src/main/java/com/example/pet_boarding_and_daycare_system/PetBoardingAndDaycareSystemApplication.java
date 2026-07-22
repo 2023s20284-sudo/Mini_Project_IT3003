cf3a7e1fa51529f735365692d0ebf26a0b22662e
2023s20284-sudo <2023s20284@stu.cmb.ac.lk>
2026-07-23 00:03:43 +0530

    Initial Spring Boot Base Setup

---
+++ src/main/java/com/example/pet_boarding_and_daycare_system/PetBoardingAndDaycareSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.pet_boarding_and_daycare_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetBoardingAndDaycareSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetBoardingAndDaycareSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pet_boarding_and_daycare_system/PetBoardingAndDaycareSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pet_boarding_and_daycare_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetBoardingAndDaycareSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
