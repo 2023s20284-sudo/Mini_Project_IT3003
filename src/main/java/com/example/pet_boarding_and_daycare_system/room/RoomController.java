348b04930f1596ade6fae4c39b514bc8e009616a
Sashini-Nawodya <2023s20252@stu.cmb.ac.lk@@example.com>
2026-07-27 10:07:03 +0530

    Add repository, service, and controller for Room module

---
+++ src/main/java/com/example/pet_boarding_and_daycare_system/room/RoomController.java
@@ -0,0 +1,4 @@
+package com.example.pet_boarding_and_daycare_system.room;
+
+public class RoomController {
+}
---
+++ src/main/java/com/example/pet_boarding_and_daycare_system/room/RoomService.java
@@ -0,0 +1,17 @@
+package com.example.pet_boarding_and_daycare_system.room;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoomService {
+
+    /*@Autowired
+    private RoomRepository roomRepository;
+
+    // Mandatory method for Dilara to fetch price & room details
+    public Room getRoomById(Long id) {
+        return roomRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Room not found with id: " + id));
+    }*/
+}
