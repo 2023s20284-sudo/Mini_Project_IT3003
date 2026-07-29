88b8f2ffb16f2412bd783e2e5198265abcbe1556
2023s20284-sudo <2023s20284@stu.cmb.ac.lk>
2026-07-29 22:50:14 +0530

    fix: move main application class issue

--- src/main/java/com/example/pet_boarding_and_daycare_system/PetBoardingAndDaycareSystemApplication.java
+++ src/main/java/com/example/pet_boarding_and_daycare_system/PetBoardingAndDaycareSystemApplication.java
@@ -1,4 +1,4 @@
-package com.example.pet_boarding_and_daycare_system.owner;
+package com.example.pet_boarding_and_daycare_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
