ac103b75cf2ffbcc6ee39554e2aba34d739f9701
2023s20284-sudo <2023s20284@stu.cmb.ac.lk>
2026-08-03 16:30:49 +0530

    fixing a delete issue

--- src/main/java/com/example/pet_boarding_and_daycare_system/review/ReviewController.java
+++ src/main/java/com/example/pet_boarding_and_daycare_system/review/ReviewController.java
@@ -1,6 +1,7 @@
 package com.example.pet_boarding_and_daycare_system.review;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -37,8 +38,15 @@ public class ReviewController {
     public List<Review> getTopReviews(@RequestParam(defaultValue = "4") int minRating) {
         return reviewService.getTopReviews(minRating);
     }
+
     @PutMapping("/{id}")
     public Review updateReview(@PathVariable Long id, @RequestBody Review review) {
         return reviewService.updateReview(id, review);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReview(@PathVariable Long id) {
+        reviewService.deleteReview(id);
+        return ResponseEntity.noContent().build(); // HTTP 204
     }
 }
